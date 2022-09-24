13e0e9dcf8b4540679a2ed8040e956a09eee9101
Sanjiv Patel <sanjivpatel@sanjivs-mbp.dyndns.rice.edu>
2022-09-23 23:52:15 -0500

    added action bar buttons, have yet to implement onClick for them

--- app/src/main/java/com/example/walksafe/MainActivity.java
+++ app/src/main/java/com/example/walksafe/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.walksafe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater()
+                .inflate(R.menu.actionbarmenu, menu);
+        return true;
+    }
+
+
 }
