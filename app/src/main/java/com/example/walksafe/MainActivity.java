aacc1db431134d1fb701f31732a782b3490202d7
Sanjiv Patel <sanjivpatel@sanjivs-mbp.dyndns.rice.edu>
2022-09-23 23:57:11 -0500

    fixed xml formatting

--- app/src/main/java/com/example/walksafe/MainActivity.java
+++ app/src/main/java/com/example/walksafe/MainActivity.java
@@ -15,15 +15,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-<<<<<<< HEAD
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater()
                 .inflate(R.menu.actionbarmenu, menu);
         return true;
     }
-
-=======
->>>>>>> 9b5b469d178da953f042539dddf903751cd1e4d1
 
 }
