649a228643c542931d21fc1c369373b0925d1f38
Sanjiv Patel <sanjivpatel@sanjivs-mbp.dyndns.rice.edu>
2022-09-24 13:37:58 -0500

    added a phone call button to MainActivity that can make calls, added components to the CallSettingsActivity Layout and Java files

--- app/src/main/java/com/example/walksafe/CallSettingsActivity.java
+++ app/src/main/java/com/example/walksafe/CallSettingsActivity.java
@@ -3,12 +3,26 @@ package com.example.walksafe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CallSettingsActivity extends AppCompatActivity {
+
+    public static final String TAG = "CallSettingsActivity";
+    private TextView tvEmCall1;
+    private TextView tvEmCall1Description;
+    private EditText etEmCall1;
+    private Button btnSave1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_settings);
+
+        tvEmCall1 = findViewById(R.id.tvEmCall1);
+        tvEmCall1Description = findViewById(R.id.tvEmCall1Description);
+        etEmCall1 = findViewById(R.id.etEmCall1);
+        btnSave1 = findViewById(R.id.btnSave1);
     }
 }
--- app/src/main/java/com/example/walksafe/MainActivity.java
+++ app/src/main/java/com/example/walksafe/MainActivity.java
@@ -9,12 +9,50 @@ import android.view.MenuItem;
 
 import com.parse.ParseUser;
 
+// added imports for making calls
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.view.View;
+import android.widget.EditText;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    // fields for making calls
+    EditText phoneNo;
+    FloatingActionButton callbtn;
+    static int PERMISSION_CODE= 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // added functionality for making calls
+        phoneNo = findViewById(R.id.editTextPhone);
+        callbtn = findViewById(R.id.callbtn);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CALL_PHONE},PERMISSION_CODE);
+
+        }
+
+        callbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String phoneno = phoneNo.getText().toString();
+                Intent i = new Intent(Intent.ACTION_CALL);
+                i.setData(Uri.parse("tel:"+phoneno));
+                startActivity(i);
+
+            }
+        });
+
     }
 
     @Override
@@ -33,13 +71,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onCallSettings(MenuItem item) {
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        Intent intent = new Intent(MainActivity.this, CallSettingsActivity.class);
         startActivity(intent);
 //        finish();
     }
 
     public void onMessageSettings(MenuItem item) {
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        Intent intent = new Intent(MainActivity.this, MessageSettingsActivity.class);
         startActivity(intent);
 //        finish();
     }
